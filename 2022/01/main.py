31d5d8851ee44746f29d74354af37536ec956b78
Nikos Papadimitriou <nikolaosppd@gmail.com>
2022-12-05 17:16:14 +0200

    2022, day 1

---
+++ 2022/01/main.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    with open("d1.txt", "r") as file:
+        calories = file.read().splitlines()
+
+    elf_counter = 0
+    elf_total_calories = [0]
+    for line in calories:
+        if line:
+            elf_total_calories[elf_counter] += int(line)
+        else:
+            elf_counter += 1
+            elf_total_calories.append(0)
+
+    elf_total_calories.sort(reverse=True)
+    print(elf_total_calories[0] + elf_total_calories[1] + elf_total_calories[2])
